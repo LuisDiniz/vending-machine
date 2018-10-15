df5bb951ae62b396b53db58c9d29369feff51f07
Luis Diniz <luis.diniz.carvalho@gmail.com>
2018-10-15 19:15:44 -0300

    Criar script dml para inserir alguns registros nas tabelas para testes.

---
+++ scripts/dml.sql
@@ -0,0 +1,5 @@
+-- Insere registros nas tabelas para realizar testes
+INSERT INTO Cartao (Saldo) VALUES (0)
+
+INSERT INTO Produto (Descricao, Preco) VALUES ('Coca-cola', 5.50)
+INSERT INTO Produto (Descricao, Preco) VALUES ('M&M', 2.00)
